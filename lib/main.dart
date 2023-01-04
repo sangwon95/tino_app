154d23d4561afc2c8a857318798aa7aaef206aed
pagsang-won <tkddnjs2575@naver.com>
2023-01-04 17:31:53 +0900

    update kakao login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:tino_app/page/login_page.dart';
 import 'package:tino_app/utils/color.dart';
 
 void main() {
+
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: 'a572d2d93516fba942a56bdabc3eda16',
+    javaScriptAppKey: 'a3ae5ccaee5d89860815e69151622435',
+  );
+
+
   runApp(const MyApp());
 }
 
